e41a193c1facb2682250074709c495a108597d42
Igor Sedura <igorsedura@gmail.com>
2022-12-01 19:16:42 +0200

    bugfix

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { RandomColor } from 'components/untils/RandomColor';
+import { RandomColor } from 'utils/RandomColor';
 import { Card, Title, List, ListlItem } from './StatisticsStyle';
 export const Statistics = ({ stats }) => {
   return (
